a56957f4805fb562125ff2b291c791950c9ff2cc
fishii <fishii@example.com>
2012-03-31 14:22:31 +0900

    add FirstPairWork

---
+++ src/main/java/features/giraffe/FirstPairWork.java
@@ -0,0 +1,15 @@
+package features.giraffe;
+
+public class FirstPairWork implements features.FirstPairWork {
+
+	@Override
+	public String getIkigomi() {
+		return "がんばります。";
+	}
+
+	@Override
+	public String getMembers() {
+		return "みつかわ　いしい";
+	}
+
+}
